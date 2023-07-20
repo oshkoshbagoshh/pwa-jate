502fb118a5c527a887b790aa8e33853ec94c004f
AJ Javadi <amirjavadi25@gmail.com>
2023-07-19 19:21:42 -0500

    cacing set up

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,44 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) => {
+    return (
+      // CSS Stylesheet 
+      request.destination === 'style' ||
+      // JavaScript
+      request.destination === 'script' ||
+      // manifest.json
+      request.destination === 'manifest' ||
+    );
+  },
+
+  // static resource caching
+  new StaleWhileRevalidate({
+    cacheName: 'static-resources',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+
+
+);
+
+// image caching
+registerRoute(
+  ({ request }) => request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'my-image-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
